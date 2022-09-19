385c49dc2fc446f94b32d60d5c0955806494b277
Alex Via <alexandervia@gmail.com>
2022-09-19 11:24:26 -0500

    creates LoginView page and route, adds jwt authorization

--- src/main.js
+++ src/main.js
@@ -3,3 +3,10 @@ import App from './App.vue'
 import router from './router'
 
 createApp(App).use(router).mount('#app')
+
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+
+var jwt = localStorage.getItem("jwt");
+if (jwt) {
+  axios.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
+}
